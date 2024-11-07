b6b8794692ffe8f526620621c97d49e552b93756
Tyopo131 <117580158+Tyopo131@users.noreply.github.com>
2024-11-07 13:46:58 +0000

    Add unpair functionality

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+enum RadioMessage {
+    message1 = 49434
+}
 radio.onReceivedNumber(function (receivedNumber) {
     if (true) {
         if (awaitingInitResponse == true) {
@@ -12,12 +15,33 @@ radio.onReceivedNumber(function (receivedNumber) {
             radio.sendNumber(1)
         }
     }
+    if (receivedNumber == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+        if (unpaired == false) {
+            unpaired = true
+            radio.sendNumber(2)
+        }
+        awaitingInitResponse = false
+    }
+})
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    radio.sendNumber(2)
+})
+input.onButtonPressed(Button.A, function () {
+	
 })
 function init () {
     radio.setGroup(frequency)
     awaitingInitResponse = true
     radio.sendNumber(1)
 }
+let unpaired = false
 let frequency = 0
 let awaitingInitResponse = false
 awaitingInitResponse = false
