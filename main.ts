511c6e44a9a073db08025dac178da6370afec62e
Tyopo131 <117580158+Tyopo131@users.noreply.github.com>
2024-11-07 12:36:35 +0000

    Add init functionality

--- main.ts
+++ main.ts
@@ -1,3 +1,25 @@
-basic.forever(function () {
-	
+radio.onReceivedNumber(function (receivedNumber) {
+    if (true) {
+        if (awaitingInitResponse == true) {
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . . . .
+                # . . . #
+                . # # # .
+                `)
+            awaitingInitResponse = false
+            radio.sendNumber(1)
+        }
+    }
 })
+function init () {
+    radio.setGroup(frequency)
+    awaitingInitResponse = true
+    radio.sendNumber(1)
+}
+let frequency = 0
+let awaitingInitResponse = false
+awaitingInitResponse = false
+frequency = 152
+init()
